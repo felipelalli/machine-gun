3451c0a5de6be17bc8d56be2f093363a03495df6
Felipe Micaroni Lalli <micaroni@gmail.com>
2010-11-21 01:21:59 -0200

    + comments in test

--- src_test/br/fml/eti/machinegun/test/MachineGunTest.java
+++ src_test/br/fml/eti/machinegun/test/MachineGunTest.java
@@ -33,12 +33,18 @@ public class MachineGunTest {
 
     @Test
     public void test() throws Exception {
+        // create an ArmyAudit that just show to standard output
         ArmyAudit armyAudit = new SystemOutAudit();
+
+        // create a fake "persisted" queue manager. It just use BlockingQueue
         PersistedQueueManager queueManager = new VolatileQueueManager();
         ImportedWeapons importedWeapons = new ImportedWeapons(queueManager);
 
+        // create an Army to make machine guns
         final Army army = new Army(armyAudit, importedWeapons);
 
+        // here is the dirty task producer
+        // ProcessIntegerSlowly simulates a "dirty work" and just sleep randomly
         Factory<DirtyWork<Integer>> dirtyWorkFactory = new Factory<DirtyWork<Integer>>() {
             private ProcessIntegerSlowly processIntegerSlowly
                     = new ProcessIntegerSlowly();
@@ -49,18 +55,24 @@ public class MachineGunTest {
             }
         };
 
+        // This capsule uses Google Protocol Buffers to convert Integer
+        // into an array of bytes and vice-versa
         Capsule<Integer> capsule = new CapsuleInteger();
 
+        // start the mission called "default mission" working on "default queue"
         army.startNewMission("default mission", "default queue",
                 dirtyWorkFactory, capsule);
 
         Thread[] threads = new Thread[100];
 
+        // creates 100 producers
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new Thread("Producer " + i + " of " + threads.length) {
                 public void run() {
+                    // get a machine gun to strafe
                     MachineGun<Integer> machineGun = army.getANewMachineGun();
 
+                    // Produces 10.000 elements
                     for (int j = 0; j < 10000; j++) {
                         try {
                             int n = sequential++;
@@ -69,6 +81,8 @@ public class MachineGunTest {
                                     + " will produce " + n);
 
                             processed.add(n);
+
+                            // strafe
                             machineGun.fire(n, "default mission");
                         } catch (Exception e) {
                             throw new RuntimeException(e);
@@ -77,15 +91,19 @@ public class MachineGunTest {
                 }
             };
 
+            // start the producer
             threads[i].start();
         }
 
+        // wait the producers die
         for (Thread thread : threads) {
             thread.join();
         }
 
+        // make all consumers die
         army.stopTheMission("default mission");
 
+        // everything was well processed?
         Assert.assertTrue(processed.size() == 0);
     }
 }
